2016a2449b6f6cfe867a69ba67ab7a4fb0ca271c
OhWell27 <100896279+OhWell27@users.noreply.github.com>
2022-03-12 07:27:43 -0500

    Add files via upload

---
+++ nft-minting-app-main/build/precache-manifest.7b6d7e91dced728d6b7487bcb02f38fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd7a902424f1a4a901eb",
+    "url": "/static/css/main.4b6842ed.chunk.css"
+  },
+  {
+    "revision": "bd7a902424f1a4a901eb",
+    "url": "/static/js/main.bd7a9024.chunk.js"
+  },
+  {
+    "revision": "3467158de5b5aa2056c4",
+    "url": "/static/js/1.3467158d.chunk.js"
+  },
+  {
+    "revision": "052688ec823bea9a1533",
+    "url": "/static/js/2.052688ec.chunk.js"
+  },
+  {
+    "revision": "9dbd2c9f102af99b8ca8",
+    "url": "/static/js/runtime~main.9dbd2c9f.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  },
+  {
+    "revision": "607b79c298b7d7b2601046d22c06b39b",
+    "url": "/index.html"
+  }
+];
